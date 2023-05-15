e4db7f853a9a40cdab3cbc5872560dc1d02a45a5
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-05-15 21:32:32 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = batteryBatches.reduce((acumulator, current) => {
+    return acumulator + current;
+})
+
+
+// const letters = ["a", "b", "c", "b", "a", "a", "c", "b", "a", "a", "c", "e", "f", "g", "h", "u", "i", "b", "a", "a", "c", "b", "a", "a", "e", "f", "g", "h", "h", "u", "i"];
+// let totalLetters = letters.reduce((accumulator, current) => {
+//     if(accumulator[current]){
+//         accumulator[current] += 1;
+//     }else{
+//         accumulator[current] = 1;
+//     }
+//     return accumulator;
+// },{})
+// console.log(totalLetters);
